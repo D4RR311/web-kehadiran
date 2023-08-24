a5b7d40ed64357de9ee6afb82d8bfcc29107b2e5
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2023-08-24 09:58:37 +0700

    Update 4_📬_Kontak_email.py

--- pages/4_📬_Kontak_email.py
+++ pages/4_📬_Kontak_email.py
@@ -18,4 +18,4 @@ def local_css(filename):
     with open(filename) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("/pages/aset/style.css")
+local_css("pages/aset/style.css")
