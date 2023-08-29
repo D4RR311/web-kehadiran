7b6e90ac5808ce4eacc5ec934f5ca66d527d6967
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2023-08-29 19:30:16 +0700

    Update 4_📬_Kontak_email_dan_QnA.py

--- pages/4_📬_Kontak_email_dan_QnA.py
+++ pages/4_📬_Kontak_email_dan_QnA.py
@@ -51,4 +51,4 @@ def local_css(filename):
     with open(filename) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("aset/style.css")
+local_css("model/style.css")
