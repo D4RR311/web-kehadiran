db8e65d9561da94659b79b3cb080c6047cea2d93
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2023-08-29 19:27:29 +0700

    Update 4_📬_Kontak_email_dan_QnA.py

--- pages/4_📬_Kontak_email_dan_QnA.py
+++ pages/4_📬_Kontak_email_dan_QnA.py
@@ -7,20 +7,24 @@ st.set_page_config(
 
 custom_web = f"""
 <style>
+
 [data-testid="stToolbar"] {{
 right: 2rem;
 }}
 
-[data-testid="collapsedControl"]{{
+[data-testid="collapsedControl"] {{
 left: 1rem;
 }}
+
 </style>
 """
 
-st.title(" Tetap Dalam Sentuhan")
+st.title("Tetap Dalam Sentuhan")
+st.markdown(custom_web, unsafe_allow_html=True)
+st.write("📬Untuk Mengetahui Lebih Lanjut Tentang Absensi Kami, isi Email Anda Di Bawah Sini")
 
-contact_form = """
-<form action="https://formsubmit.co/darrellardhanihidayat@gmail.com" method="POST">
+kontak_form = """
+<form action="https://formsubmit.co/emailreceiver224@gmail.com" method="POST">
     <input type="hidden" name="_captcha" value="false">
     <input type="text" name="name" placeholder="Nama Anda" required>
     <input type="email" name="email" placeholder="Email Anda" required>
@@ -28,12 +32,23 @@ contact_form = """
     <button type="submit">Send</button>
 </form>
 """
+st.markdown(kontak_form, unsafe_allow_html=True)
 
-st.markdown(contact_form, unsafe_allow_html=True)
-st.markdown(custom_web, unsafe_allow_html=True)
+st.write("#")
+st.subheader("🤔Jika Ada Pertanyaan, Silahkan Ketik Email Anda dan pertanyaan anda")
+pertanyaan_form =  """
+<form action="https://formsubmit.co/emailreceiver224@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Nama Anda" required>
+    <input type="email" name="email" placeholder="Email Anda" required>
+    <textarea name="message" placeholder="Orang Tua Dari Kelas dan pertanyaan anda"></textarea>
+    <button type="submit">Send</button>
+</form>
+"""
+st.markdown(pertanyaan_form, unsafe_allow_html=True)
 
 def local_css(filename):
     with open(filename) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("web/kehadiran/pages/aset/style.css")
+local_css("pages/aset/style.css")
