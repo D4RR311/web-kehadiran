2f7e454f6127ba671d65c4f60d21f698aea53ca8
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2023-08-29 18:31:24 +0700

    Update 4_📬_Kontak_email.py

--- pages/4_📬_Kontak_email.py
+++ pages/4_📬_Kontak_email.py
@@ -45,7 +45,7 @@ pertanyaan_form =  """
 </form>
 """
 st.markdown(kontak_form, unsafe_allow_html=True)
-st.markdown(pertanyaan_form, unsafe_allow_hmtl=True)
+st.markdown(pertanyaan_form, unsafe_allow_html=True)
 
 def local_css(filename):
     with open(filename) as f:
