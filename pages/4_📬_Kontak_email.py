a7c33c1e3e89bee53cbfd77737f52bbfa2e06b74
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2023-08-29 18:06:13 +0700

    Update 4_📬_Kontak_email.py

--- pages/4_📬_Kontak_email.py
+++ pages/4_📬_Kontak_email.py
@@ -23,7 +23,7 @@ st.title("Tetap Dalam Sentuhan")
 st.markdown(custom_web, unsafe_allow_html=True)
 st.write("Untuk Mengetahui Lebih Lanjut Tentang Absensi Kami, isi Email Anda Di Bawah Sini")
 
-contact_form = """
+kontak_form = """
 <form action="https://formsubmit.co/emailreceiver224@gmail.com" method="POST">
     <input type="hidden" name="_captcha" value="false">
     <input type="text" name="name" placeholder="Nama Anda" required>
@@ -32,8 +32,20 @@ contact_form = """
     <button type="submit">Send</button>
 </form>
 """
-
 st.markdown(contact_form, unsafe_allow_html=True)
+
+st.write("#")
+st.subheader("Jika Ada Pertanyaan, Silahkan Ketik Email Anda dan pertanyaan anda")
+pertanyaan_form =  """
+<form action="https://formsubmit.co/emailreceiver224@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Nama Anda" required>
+    <input type="email" name="email" placeholder="Email Anda" required>
+    <textarea name="message" placeholder="Orang Tua Dari Kelas dan pertanyaan anda"></textarea>
+    <button type="submit">Send</button>
+</form>
+"""
+st.markdown(pertanyaan_from, unsafe_allow_hmtl=True)
 
 def local_css(filename):
     with open(filename) as f:
