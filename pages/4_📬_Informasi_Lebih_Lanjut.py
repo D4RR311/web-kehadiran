8d2c09209b12e12e27094af264a172ffd8f16f99
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2023-12-10 17:33:36 +0700

    Update 4_📬_Informasi_Lebih_Lanjut.py

--- pages/4_📬_Informasi_Lebih_Lanjut.py
+++ pages/4_📬_Informasi_Lebih_Lanjut.py
@@ -17,4 +17,4 @@ st.markdown(form_kontak, unsafe_allow_html=True)
 def local_css(file_name):
      with open(file_name) as f:
           st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("model/style.css")
+local_css("pages/model/style.css")
