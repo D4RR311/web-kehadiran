d79e6ba96f0866fd72efa554d5a209ab39bfc37b
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2023-12-10 16:50:07 +0700

    Update 4_📬_Informasi_Lebih_Lanjut.py

--- pages/4_📬_Informasi_Lebih_Lanjut.py
+++ pages/4_📬_Informasi_Lebih_Lanjut.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.header(":mailbox: Info Lebih Lanjut")
+
+form_kontak = """
+<form action="https://formsubmit.co/emailreceiver@email.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(form_kontak, unsafe_allow_html=True)
