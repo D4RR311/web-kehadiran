76af4495dbf369d5f9eb8cfe3c45000b78be05b7
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2024-08-07 15:09:00 +0700

    Update 4_📬_Informasi_Lebih_Lanjut.py

--- pages/4_📬_Informasi_Lebih_Lanjut.py
+++ pages/4_📬_Informasi_Lebih_Lanjut.py
@@ -7,15 +7,87 @@ st.set_page_config(
 
 st.header(":mailbox: Info Lebih Lanjut")
 
-st.write("Jika ada pertanyaan atau saran, silahkan isi forum dibawah ini")
+st.write("Jika ada pertanyaan atau saran, silahkan isi formulir di bawah ini")
 
 form_kontak = """
-<form action="https://formsubmit.co/receiveremail612@gmail.com" method="POST">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your message here"></textarea>
-     <button type="submit">Send</button>
-</form>
+<iframe
+    id="JotFormIFrame-2421911427801452"
+    title="JotForm Form"
+    onload="window.parent.scrollTo(0,0)"
+    allowtransparency="true"
+    allowfullscreen="true"
+    allow="geolocation; microphone; camera"
+    src="https://form.jotform.com/2421911427801452"
+    frameborder="0"
+    style="min-width:100%;height:539px;border:none;"
+    scrolling="no"
+></iframe>
+<script type="text/javascript">
+    var ifr = document.getElementById("JotFormIFrame-2421911427801452");
+    if (ifr) {
+        var src = ifr.src;
+        var iframeParams = [];
+        if (window.location.href && window.location.href.indexOf("?") > -1) {
+            iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
+        }
+        if (src && src.indexOf("?") > -1) {
+            iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
+            src = src.substr(0, src.indexOf("?"));
+        }
+        iframeParams.push("isIframeEmbed=1");
+        ifr.src = src + "?" + iframeParams.join('&');
+    }
+    window.handleIFrameMessage = function(e) {
+        if (typeof e.data === 'object') { return; }
+        var args = e.data.split(":");
+        if (args.length > 2) { iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]); } 
+        else { iframe = document.getElementById("JotFormIFrame"); }
+        if (!iframe) { return; }
+        switch (args[0]) {
+            case "scrollIntoView":
+                iframe.scrollIntoView();
+                break;
+            case "setHeight":
+                iframe.style.height = args[1] + "px";
+                break;
+            case "collapseErrorPage":
+                if (iframe.clientHeight > window.innerHeight) {
+                    iframe.style.height = window.innerHeight + "px";
+                }
+                break;
+            case "reloadPage":
+                window.location.reload();
+                break;
+            case "loadScript":
+                    if( typeof args[1] == 'string' && args[1].indexOf("http") === 0 && !e.origin.indexOf("jotform") ) {
+                    var src = args[1].split("://")[1];
+                    if (window.location.href.indexOf(src) > -1) { break; }
+                    var script = document.createElement('script');
+                    script.src = args[1];
+                    script.type = "text/javascript";
+                    document.body.appendChild(script);
+                }
+                break;
+            case "exitFullscreen":
+                if      (window.document.exitFullscreen)        window.document.exitFullscreen();
+                else if (window.document.mozCancelFullScreen)   window.document.mozCancelFullScreen();
+                else if (window.document.mozCancelFullscreen)   window.document.mozCancelFullScreen();
+                else if (window.document.webkitExitFullscreen)  window.document.webkitExitFullscreen();
+                else if (window.document.msExitFullscreen)      window.document.msExitFullscreen();
+                break;
+        }
+        var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+        if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+            var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
+            iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*");
+        }
+    };
+    if (window.addEventListener) {
+        window.addEventListener("message", handleIFrameMessage, false);
+    } else if (window.attachEvent) {
+        window.attachEvent("onmessage", handleIFrameMessage);
+    }
+</script>
 """
 
 st.markdown(form_kontak, unsafe_allow_html=True)
