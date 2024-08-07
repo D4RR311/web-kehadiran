319f65904514baf49374b2945d70dd078392268a
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2024-08-07 12:43:49 +0700

    Update 4_📬_Informasi_Lebih_Lanjut.py

--- pages/4_📬_Informasi_Lebih_Lanjut.py
+++ pages/4_📬_Informasi_Lebih_Lanjut.py
@@ -11,11 +11,10 @@ st.write("Jika ada pertanyaan atau saran, silahkan isi forum dibawah ini")
 
 form_kontak = """
 <form action="https://formsubmit.co/receiveremail612@gmail.com" method="POST">
-     <input type="hidden" name="_captcha" value="false">
      <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name"email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your message here"></teaxtarea>
-     <button type="submit">send</button>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
 </form>
 """
 
@@ -24,6 +23,5 @@ st.markdown(form_kontak, unsafe_allow_html=True)
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
 
 local_css("pages/model/style.css")
