547418ba075ce5d7175879a851ab39772d2e941a
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2024-08-07 12:36:31 +0700

    Update 4_📬_Informasi_Lebih_Lanjut.py

--- pages/4_📬_Informasi_Lebih_Lanjut.py
+++ pages/4_📬_Informasi_Lebih_Lanjut.py
@@ -11,6 +11,7 @@ st.write("Jika ada pertanyaan atau saran, silahkan isi forum dibawah ini")
 
 form_kontak = """
 <form action="https://formsubmit.co/receiveremail612@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
      <input type="text" name="name" placeholder="Your name" required>
      <input type="email" name"email" placeholder="Your email" required>
      <textarea name="message" placeholder="Your message here"></teaxtarea>
