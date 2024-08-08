e46e3ea0b813e38ceb9b12ed52ef60bb8be8feba
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2024-08-08 08:45:20 +0700

    Update 4_📬_Informasi_Lebih_Lanjut.py

--- pages/4_📬_Informasi_Lebih_Lanjut.py
+++ pages/4_📬_Informasi_Lebih_Lanjut.py
@@ -52,7 +52,7 @@ form_kontak = """
     allow="geolocation; microphone; camera"
     src="https://form.jotform.com/242191427801452"
     frameborder="0"
-    style="min-width:120%;height:1307px;border:none;"
+    style="min-width:120%;height:1310px;border:none;"
     scrolling="no"
 ></iframe>
 <script type="text/javascript">
