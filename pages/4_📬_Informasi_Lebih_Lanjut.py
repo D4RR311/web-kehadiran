d1b572adea9b5f2cfce894cc3b6b6aad3d2bbd25
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2024-08-08 07:04:02 +0700

    Update 4_📬_Informasi_Lebih_Lanjut.py

--- pages/4_📬_Informasi_Lebih_Lanjut.py
+++ pages/4_📬_Informasi_Lebih_Lanjut.py
@@ -1,9 +1,18 @@
 import streamlit as st
+import base64
 
 st.set_page_config(
      page_title="Web Absensi",
      page_icon="🗓️",
 )
+
+@st.cache_data
+def get_img_as_base64(file):
+     with open(file, "rb") as f:
+          data = f.read()
+     return base64.b64encode(data).decode()
+
+img = get_img_as_base64("foto/SAKASMA.jpg")
 
 st.header(":mailbox: Info Lebih Lanjut")
 
