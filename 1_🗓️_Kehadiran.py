d60b0cb40eae9ed0c91b2af5ea0c51c0db40317e
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2025-01-02 15:48:22 +0700

    Update 1_🗓️_Kehadiran.py

--- 1_🗓️_Kehadiran.py
+++ 1_🗓️_Kehadiran.py
@@ -1,17 +1,16 @@
+import pandas as pd
 import base64
 import streamlit as st
-import pandas as pd
-import time
-from datetime import datetime
+from streamlit_gsheets import GSheetsConnection
 
 st.set_page_config(
     page_title="Web Absensi",
-    page_icon="🗓️",
+    page_icon="📆"
 )
 
 @st.cache_data
 def get_img_as_base64(file):
-    with open(file, "rb") as f:
+    with open(file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
@@ -24,27 +23,33 @@ background-image: url("data:image/png;base64,{img}");
 background-size: cover;
 }}
 
-[data-testid="stHeader"] {{
+[data-testid="stHeader"]{{
 background-color: rgba(0, 0, 0, 0);
 }}
 
-[data-testid="stToolbar"] {{
-right: 1rem;
-}}
-
-[data-testid="collapsedControl"] {{
+[data-testid="collapseControl"]{{
 left: 1rem;
 }}
 
 </style>
 """
-
-st.title("Bagian Absensi")
 st.markdown(bg_web, unsafe_allow_html=True)
+url = "https://docs.google.com/spreadsheets/d/1OxwoDBP6eDowycuYtNP5_vyvM_j8QBmfu78o_Y8g6XI/edit?usp=sharing"
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(spreadsheet=url, usecols=[0, 1, 2])
 
-sw = time.time()
-tanggal = datetime.fromtimestamp(sw).strftime("%d-%m-%Y")
-stempel_waktu = datetime.fromtimestamp(sw).strftime("%H-%M-%S")
-df = pd.read_csv("absensi/kehadiran_" + tanggal + ".csv")
+col1, col2 = st.coolumns(2)
+with col1:
+    st.dataframe(df)
+with col2:
+    st.header("Filter Kelas:")
 
-st.dataframe(df.style.highlight_max(axis=0))
+    kelas = st.multiselect(
+        "Pilih Kelas:",
+        options=df["KELAS"].dropna().unique()
+        default=df["KELAS"].dropna().unique()
+    )
+    df_selection = df.query(
+        "KELAS == @kelas"
+    )
+    st.dataframe(df_selection)
