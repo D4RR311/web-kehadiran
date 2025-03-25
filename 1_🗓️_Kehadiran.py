591e0469c4828644ff701d8531995b787b614598
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2025-03-25 10:11:06 +0700

    Update 1_🗓️_Kehadiran.py

--- 1_🗓️_Kehadiran.py
+++ 1_🗓️_Kehadiran.py
@@ -40,7 +40,7 @@ left: 1rem;
 """
 st.title("Daftar Hadir Siswa")
 st.markdown(bg_web, unsafe_allow_html=True)
-url = "https://docs.google.com/spreadsheets/d/1VYDKl3DMeH1OBLfkfg_Yq3bWkquFvN_iShI-DVUvydc/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/1TmPb_i4Yt5abu_cFCwd4S41L5CEKMSjtU0YONdMFyPQ/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(spreadsheet=url, usecols=[0, 1, 2])
 
